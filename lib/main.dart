5183d44d12c99fe5961f07172272e9f707431f03
Prince Narteh <prinarteh@gmail.com>
2022-07-24 13:51:14 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(244, 243, 242, 1),
+      appBar: AppBar(),
+      body: const Center(
+        child: Text("Inspirational App"),
+      ),
+    );
+  }
+}
