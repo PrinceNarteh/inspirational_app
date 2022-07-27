dd5951e625bac5c954c0a7d2435ee68379b76511
Prince Narteh <prinarteh@gmail.com>
2022-07-27 06:05:13 +0000

    creates promo card widget file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:inspiration_app/widgets/promo_card.dart';
 
 void main() {
   runApp(
@@ -113,37 +114,6 @@ class HomeScreen extends StatelessWidget {
               ),
             )
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget promoCard(image) {
-    return AspectRatio(
-      aspectRatio: 2.62 / 3,
-      child: Container(
-        margin: const EdgeInsets.only(right: 15.0),
-        decoration: BoxDecoration(
-          color: Colors.orange,
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(image),
-          ),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                  begin: Alignment.bottomRight,
-                  stops: const [
-                    0.1,
-                    0.9
-                  ],
-                  colors: [
-                    Colors.black.withOpacity(.8),
-                    Colors.black.withOpacity(.1)
-                  ])),
         ),
       ),
     );
